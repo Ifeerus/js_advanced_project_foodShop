4d7f60e00e26b30d1646c7b5fe7b4b9705b1eff9
Igor Franchiuc <ifeerus@gmail.com>
2022-12-25 13:55:07 +0200

    Adding another variant of function of getting data!

--- js/script.js
+++ js/script.js
@@ -210,6 +210,31 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         });
 
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => createCard(data));
+
+    // function createCard(data) {
+    //     data.forEach(({img, altimg, title, descr, price}) => {
+    //         const element = document.createElement('div');
+
+    //         element.classList.add('menu__item');
+
+    //         element.innerHTML = `
+    //             <img src=${img} alt=${altimg}>
+    //             <h3 class="menu__item-subtitle">${title}</h3>
+    //             <div class="menu__item-descr">${descr}</div>
+    //             <div class="menu__item-divider"></div>
+    //             <div class="menu__item-price">
+    //                 <div class="menu__item-cost">Цена:</div>
+    //                 <div class="menu__item-total"><span>${price}</span> мдл/день</div>
+    //             </div>
+    //         `;
+
+    //         document.querySelector('.menu .container').append(element);
+    //     })
+    // }
+    
+
     //Отправка данных с форм (XML HTTP request - устаревший вариант)
 
     const forms = document.querySelectorAll('form');
